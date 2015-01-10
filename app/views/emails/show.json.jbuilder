b31b5db1920efd468f3c7f33f9e0d71d3916544d
ivanljc <ivanljc@gmail.com>
2015-01-10 00:35:47 -0800

    Generated emails database and stylistic changes

---
+++ app/views/emails/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @email, :id, :name, :email, :subject, :body, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :emails
+
     #get 'pages/home'
     root 'pages#home'
 
